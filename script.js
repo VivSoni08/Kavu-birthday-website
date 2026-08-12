7cd4722c9a8ad0e2dc0da1e7bd9de30bb49f053d
VivSoni08 <90632088+VivSoni08@users.noreply.github.com>
2026-08-12 15:17:37 +0200

    Add memories array for image storage

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ let cameraOn = false;
 let starting = false;
 let photoNumber = 0;
 
+const memories = [
+    "images/test-1.svg",
+    "images/test-2.svg",
+    "images/test-3.svg"
+];
+
+const memoryImage = document.querySelector("#memory-image");
+
 
 // =========================
 // POWER BUTTON
